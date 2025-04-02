70a118b6943e7f35bd8b00b871109ba855cb150f
Inês Silva <inesoaresilva@gmail.com>
2025-04-02 15:37:57 -0300

    Add full-text search scope for recipes and ingredients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,11 @@
 class Recipe < ApplicationRecord
+    include PgSearch::Model
+    
+    pg_search_scope :search_by_title_and_ingredients,
+        against: [:title],
+        associated_against: { ingredients: [:name] },
+        using: { tsearch: { prefix: true } }
+
     has_many :recipe_ingredients, dependent: :destroy
     has_many :ingredients, through: :recipe_ingredients
 end
